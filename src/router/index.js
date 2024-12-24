60ae69c3460c1ab44a5eeb7e56445eb99c867794
sangmi Park <miyad927@naver.com>
2024-12-24 14:31:10 +0900

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import MainView from "../pages/common/Main.vue";
 import Stores from "../pages/store/Stores.vue";
+import Login from "../pages/user/LoginView.vue";
+import Join from "../pages/user/Join.vue";
+import JoinAgreement from "../pages/user/JoinAgreement.vue";
 
 const routes = [
   { path: "/", component: MainView },

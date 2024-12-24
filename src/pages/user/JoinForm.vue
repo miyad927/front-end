a8ea785bd7ded8bfdf237085c3b23ebfe378c148
sangmi Park <miyad927@naver.com>
2024-12-24 17:54:31 +0900

    [Fix] 회원가입 등 추가

---
+++ src/pages/user/JoinForm.vue
@@ -0,0 +1,497 @@
+<script setup></script>
+<template>
+  <div class="main">
+    <h2>회원정보 입력</h2>
+    <form
+      id="formJoin"
+      name="formJoin"
+      action=""
+      method="post"
+      class="join_frm"
+      autocomplete="off"
+      novalidate="novalidate"
+      data-gtm-form-interact-id="0"
+    >
+      <div class="join_view">
+        <section class="join_form_tabarea base_info_box join_common">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>회원타입</strong>
+                  <div class="user_type_area">
+                    <input
+                      type="radio"
+                      id="userType_customer"
+                      name="userType"
+                      value="customer"
+                    />
+                    <label for="userType_customer">소비자</label>
+                    <input
+                      type="radio"
+                      id="userType_seller"
+                      name="userType"
+                      value="seller"
+                    />
+                    <label for="userType_seller">점주</label>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>아이디</strong>
+                  <div class="member_warning">
+                    <input
+                      type="text"
+                      id="memId"
+                      name="memId"
+                      data-pattern="gdMemberId"
+                      maxlength="40"
+                      placeholder="아이디"
+                      value=""
+                      tabindex="6"
+                    />
+                  </div>
+                </td>
+              </tr>
+
+              <tr class="member_password">
+                <td>
+                  <strong>비밀번호</strong>
+                  <div class="member_warning">
+                    <input
+                      type="password"
+                      id="newPassword"
+                      name="memPw"
+                      placeholder="비밀번호"
+                      autocomplete="new-password"
+                      tabindex="7"
+                    />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="re_pw">
+                  <div class="member_warning">
+                    <input
+                      type="password"
+                      class="check-id"
+                      name="memPwRe"
+                      autocomplete="off"
+                      placeholder="비밀번호 확인"
+                      tabindex="8"
+                    />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>이름</strong>
+                  <div>
+                    <input
+                      type="text"
+                      id=""
+                      name=""
+                      maxlength="40"
+                      placeholder="이름"
+                      value=""
+                    />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>생년월일</strong>
+                  <div>
+                    <input
+                      type="text"
+                      id=""
+                      name=""
+                      maxlength="40"
+                      placeholder="생년월일"
+                      value=""
+                    />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>이메일</strong>
+                  <div class="member_email">
+                    <div class="member_warning">
+                      <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        value=""
+                        placeholder="이메일"
+                        autocomplete="off"
+                        tabindex="9"
+                      />
+                      <select
+                        id="emailDomain"
+                        name="emailDomain"
+                        class="chosen-select"
+                      >
+                        <option value="self">직접입력</option>
+                        <option value="naver.com">naver.com</option>
+                        <option value="hanmail.net">hanmail.net</option>
+                        <option value="daum.net">daum.net</option>
+                        <option value="nate.com">nate.com</option>
+                        <option value="hotmail.com">hotmail.com</option>
+                        <option value="gmail.com">gmail.com</option>
+                        <option value="icloud.com">icloud.com</option>
+                      </select>
+                    </div>
+                    <div class="member_warning js_email"></div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>주소</strong>
+                  <div>
+                    <input
+                      type="text"
+                      id=""
+                      name=""
+                      maxlength="40"
+                      placeholder="주소"
+                      value=""
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      id=""
+                      name=""
+                      maxlength="40"
+                      placeholder="상세주소"
+                      value=""
+                    />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="member_address">
+                  <strong>휴대폰 번호</strong>
+                  <div class="address_postcode">
+                    <input
+                      type="text"
+                      id="cellPhone"
+                      name="cellPhone"
+                      maxlength="12"
+                      placeholder="- 없이 입력하세요."
+                      data-pattern="gdNum"
+                      value="01012345678"
+                    />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <!-- 회원가입/정보 기본정보 -->
+      </div>
+      <div class="btn_center_box">
+        <button type="button" id="btnCancel" class="btn_cancel" tabindex="18">
+          취소
+        </button>
+        <button type="button" class="js_btn_join btn_ok" tabindex="17">
+          회원가입
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+<style scoped>
+.main {
+  width: 50%;
+  min-width: 600px;
+  margin: 0 auto;
+}
+
+.location_wrap {
+  position: relative;
+  width: 100%;
+}
+
+.location_wrap .location_cont {
+  width: 100%;
+  margin: 0;
+  text-align: right;
+}
+
+.location_cont em {
+  display: inline-block;
+  padding: 12px 0 10px 0;
+  color: #777777;
+  font-size: 12.5008px;
+}
+
+.location_cont .local_home {
+  color: #777777;
+}
+
+.location_wrap .location_cont:after {
+  display: block;
+  clear: both;
+  content: "";
+}
+
+.restriction {
+  background-color: #f9f9f9;
+  padding: 1.25rem;
+  border-radius: 0.625rem;
+  text-align: left;
+  font-size: 1rem;
+  line-height: 1.625rem;
+  margin-bottom: 1.875rem;
+}
+
+.restriction strong {
+  color: #da0000;
+  font-weight: 500;
+}
+
+h2 {
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin: 0 0 1.875rem;
+}
+
+.join_frm {
+  display: flex;
+  flex-direction: column;
+  gap: 1.875rem;
+}
+
+.join_view {
+  padding: 1.25rem 0;
+  display: flex;
+  flex-direction: column;
+  gap: 3.125rem;
+}
+
+.join_common {
+  width: 100%;
+  text-align: left;
+}
+
+.join_common h3 {
+  display: block;
+  border-bottom: 0.0625rem solid #555;
+  padding: 0 0 1.125rem;
+  margin: 0 0 0.9375rem;
+  font-size: 1.25rem;
+  font-weight: 500;
+  color: #333;
+}
+
+.join_common table {
+  width: 100%;
+  border-spacing: 0;
+}
+
+.join_common table td {
+  display: flex;
+  flex-direction: column;
+  padding: 0.9375rem 0;
+  gap: 0.625rem;
+}
+
+.join_common table strong {
+  font-size: 1rem;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  gap: 0.4375rem;
+}
+
+.join_common table td select {
+  width: 100%;
+}
+
+.member_warning {
+  display: inline;
+  position: relative;
+}
+
+.join_common table .member_warning {
+  display: flex !important
+;
+  gap: 0.625rem;
+}
+
+.join_common table .chosen-select {
+  width: 30%;
+}
+
+.join_common table input[type="text"],
+.join_common table input[type="password"] {
+  font-size: 1rem;
+  width: 100%;
+  height: 3.375rem;
+  border: 0.0625rem solid #e8e8e8;
+  padding: 0 1.125rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  margin: 0 !important;
+  font-weight: 400;
+  color: #333;
+  flex-grow: 1;
+}
+
+.join_common table td.busi_no_caution {
+  border-radius: 0.5rem;
+  padding: 1.125rem;
+  background-color: #fff0f1;
+  color: #fa3848;
+  font-size: 1rem;
+  line-height: 1.625rem;
+}
+
+.join_common table td.busi_no_caution strong {
+  display: inline;
+  font-weight: 700;
+}
+
+.join_common table strong .btn_service_detail {
+  height: 2rem;
+  border: none;
+  background-color: #f4f4f4;
+  padding: 0 0.75rem;
+  border-radius: 1rem;
+  color: #555;
+  font-size: 0.8438rem;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  gap: 0.3125rem;
+  letter-spacing: -0.05em;
+}
+
+.join_common table strong .btn_service_detail:hover {
+  background-color: #e8e8e8;
+}
+
+.join_common table strong .btn_service_detail img {
+  opacity: 0.4;
+}
+
+.join_common table select:required:invalid {
+  color: #aaa;
+}
+
+.join_common table select {
+  font-size: 1rem;
+  height: 3.375rem;
+  border: 0.0625rem solid #e8e8e8;
+  padding: 0 1.125rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  margin: 0;
+  width: 100%;
+  color: #333;
+  background: url(https://thenaum.cdn-nhncommerce.com/data/img/allnew/layout/arrow_down_wide.svg)
+    no-repeat;
+  background-size: 0.75rem 0.375rem;
+  background-position: right 1.5rem center;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+.join_common table select option {
+  color: #333;
+}
+
+.join_common table .friend_benefit {
+  background: rgba(31, 158, 222, 0.1);
+  border-radius: 0.5rem;
+  padding: 1.125rem;
+  color: #1f9ede;
+  font-size: 1rem;
+  line-height: 1.625rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.join_common table .friend_benefit strong {
+  font-weight: 700;
+  line-height: 1.625rem;
+}
+
+.join_common table #smsFl + label .sub {
+  color: #1f9ede;
+  font-size: 0.875rem;
+  display: block;
+  text-indent: 0;
+  line-height: 1.5rem;
+}
+
+.join_common table input[type="checkbox"] + label {
+  font-size: 1rem;
+  line-height: 1.5rem;
+  text-indent: -2rem;
+  padding-left: 2rem;
+  display: inline-block;
+  cursor: pointer;
+}
+
+.join_common table input[type="checkbox"] + label:before {
+  position: static;
+  width: 1.5rem;
+  height: 1.5rem;
+  background-color: #fff;
+  border: 0.0625rem solid #e8e8e8;
+  border-radius: 0.1875rem;
+  margin: 0 0.5rem 0 0;
+  box-sizing: border-box;
+  display: inline-block;
+  vertical-align: top;
+}
+
+.btn_center_box {
+  display: flex;
+  gap: 1.25rem;
+}
+
+.btn_center_box button {
+  flex-grow: 1;
+  flex-basis: 0;
+  height: 3.5rem;
+  border-radius: 0.5rem;
+  font-size: 1.125rem;
+  font-weight: 500;
+}
+
+.btn_center_box .btn_cancel {
+  border: 0.0625rem solid #e8e8e8;
+}
+
+.btn_center_box .btn_ok {
+  background-color: #00a7b3;
+  color: #fff;
+}
+
+.user_type_area {
+  display: flex;
+  column-gap: 1.25rem;
+  font-size: 1rem;
+}
+
+@media (max-width: 960px) {
+  .main {
+    min-width: 25rem;
+    max-width: 35rem;
+  }
+
+  .join_common table .member_warning {
+    flex-direction: column;
+  }
+  .join_common table .member_warning select {
+    width: 100%;
+  }
+}
+</style>
--- src/pages/user/LoginView.vue
+++ src/pages/user/LoginView.vue
@@ -13,148 +13,94 @@
             <div class="member_tit">
               <h2>로그인</h2>
             </div>
-            <form
-              id="formLogin"
-              method="post"
-              action="https://www.foodingfactory.com/member/login_ps.php"
-              class="login_frm"
-              novalidate="novalidate"
-            >
-              <input type="hidden" id="mode" name="mode" value="login" />
-              <input
-                type="hidden"
-                id="returnUrl"
-                name="returnUrl"
-                value="https%3A%2F%2Fwww.foodingfactory.com%2Fgoods%2Fbest.php"
-              />
-              <div class="member_login_box">
-                <div class="login_input_sec">
-                  <input
-                    type="text"
-                    id="loginId"
-                    name="loginId"
-                    value=""
-                    placeholder="ID"
-                    required="true"
-                    aria-required="true"
-                  />
-                  <input
-                    type="password"
-                    id="loginPwd"
-                    name="loginPwd"
-                    value=""
-                    placeholder="PASSWORD"
-                    required="true"
-                    aria-required="true"
-                  />
-                </div>
-                <div class="id_chk">
-                  <span class="form_element">
-                    <input
-                      type="checkbox"
-                      id="saveId"
-                      name="saveId"
-                      value="y"
-                      checked=""
-                    />
-                    <label for="saveId" class="">아이디 저장</label>
-                  </span>
-                  <span class="form_element">
-                    <input
-                      type="checkbox"
-                      class="fd_checkbox"
-                      name="saveAutoLogin"
-                      value="y"
-                      id="saveAutoLogin"
-                      checked="checked"
-                    />
-                    <label for="saveAutoLogin">자동로그인</label>
-                  </span>
-                  <p class="dn js_caution_msg1">
-                    아이디, 비밀번호가 일치하지 않습니다. 다시 입력해 주세요.
-                  </p>
-                </div>
-                <button type="submit">로그인</button>
-                <ul class="btn_login_box">
-                  <!-- <li>
-                        <button id="btnFindId" class="btn_member_white_new">
-                          아이디 찾기
-                        </button>
-                      </li>
-                      <li>
-                        <button id="btnFindPwd" class="btn_member_white_new">
-                          비밀번호 찾기
-                        </button>
-                      </li> -->
-                  <li>
-                    <a href="./Join" class="btn_member_join_new"> 회원가입 </a>
-                  </li>
-                </ul>
+            <div class="member_login_box">
+              <div class="login_input_sec">
+                <input
+                  type="text"
+                  id="loginId"
+                  name="loginId"
+                  value=""
+                  placeholder="ID"
+                  v-model="loginData.id"
+                />
+                <input
+                  type="password"
+                  id="loginPwd"
+                  name="loginPwd"
+                  value=""
+                  placeholder="PASSWORD"
+                  v-model="loginData.password"
+                />
               </div>
-              <!-- //login_box -->
-
-              <div class="easy_login">
-                <h3>SNS 간편 로그인</h3>
-                <div class="sns_list">
-                  <a
-                    href="#"
-                    class="kakao_login js_btn_kakao_login"
-                    data-kakao-type="login"
-                    data-return-url="https%3A%2F%2Fwww.foodingfactory.com%2Fgoods%2Fbest.php"
-                  >
-                    <img
-                      src="https://thenaum.cdn-nhncommerce.com/data/skin/front/moment/img/member/icon_kakao.png"
-                      alt="카카오 아이디 로그인"
-                    />
-                  </a>
-                  <a
-                    href="#"
-                    class="naver_login js_btn_naver_login"
-                    data-naver-url="https://socialmember.godo.co.kr/NaverLogin/naver_api.php?mode=login&amp;response_type=code&amp;client_id=JFIziYYgMBSYpY5KjrY9&amp;redirect_uri=https%3A%2F%2Fwww.foodingfactory.com%2Fgoods%2Fbest.php&amp;state=9cd077e6ebb76da5ccf25819b39b6736"
-                  >
-                    <img
-                      src="https://thenaum.cdn-nhncommerce.com/data/skin/front/moment/img/member/icon_naver.png"
-                      alt="naver 아이디 로그인"
-                    />
-                  </a>
-
-                  <a href="#" class="btn_apple_login js_btn_apple_login">
-                    <img
-                      id="apple_btn"
-                      src="https://thenaum.cdn-nhncommerce.com/data/skin/front/moment/img/member/icon_apple.png"
-                      alt="Apple 아이디 로그인"
-                    />
-                  </a>
-                </div>
-              </div>
-            </form>
-            <form
-              id="formOrderLogin"
-              action="../member/member_ps.php"
-              method="post"
-              novalidate="novalidate"
-            >
-              <input type="hidden" name="mode" value="guestOrder" />
-              <input
-                type="hidden"
-                name="returnUrl"
-                value="../mypage/order_view.php"
-              />
-              <div class="nonmember_order_box">
-                <h3>비회원 주문조회</h3>
-                <div class="order_input_sec">
-                  <div class="input_sec">
-                    <input type="text" name="orderNm" placeholder="주문자명" />
-                    <input type="text" name="orderNo" placeholder="주문번호" />
-                  </div>
-                  <button type="submit">확인</button>
-                </div>
-                <p class="js_caution_msg2">
-                  주문번호와 비밀번호를 잊으신 경우, 고객센터로 문의하여 주시기
-                  바랍니다.
+              <div class="id_chk">
+                <span class="form_element">
+                  <input
+                    type="checkbox"
+                    id="saveId"
+                    name="saveId"
+                    value="y"
+                    checked=""
+                  />
+                  <label for="saveId" class="">아이디 저장</label>
+                </span>
+                <span class="form_element">
+                  <input
+                    type="checkbox"
+                    class="fd_checkbox"
+                    name="saveAutoLogin"
+                    value="y"
+                    id="saveAutoLogin"
+                    checked="checked"
+                  />
+                  <label for="saveAutoLogin">자동로그인</label>
+                </span>
+                <p class="dn js_caution_msg1">
+                  아이디, 비밀번호가 일치하지 않습니다. 다시 입력해 주세요.
                 </p>
               </div>
-            </form>
+              <button class="btn_submit" @click="login">로그인</button>
+              <ul class="btn_login_box">
+                <li>
+                  <a href="./Join" class="btn_member_join_new"> 회원가입 </a>
+                </li>
+              </ul>
+            </div>
+            <!-- //login_box -->
+
+            <div class="easy_login">
+              <h3>SNS 간편 로그인</h3>
+              <div class="sns_list">
+                <a
+                  href="#"
+                  class="kakao_login js_btn_kakao_login"
+                  data-kakao-type="login"
+                  data-return-url="https%3A%2F%2Fwww.foodingfactory.com%2Fgoods%2Fbest.php"
+                >
+                  <img
+                    src="https://thenaum.cdn-nhncommerce.com/data/skin/front/moment/img/member/icon_kakao.png"
+                    alt="카카오 아이디 로그인"
+                  />
+                </a>
+                <a
+                  href="#"
+                  class="naver_login js_btn_naver_login"
+                  data-naver-url="https://socialmember.godo.co.kr/NaverLogin/naver_api.php?mode=login&amp;response_type=code&amp;client_id=JFIziYYgMBSYpY5KjrY9&amp;redirect_uri=https%3A%2F%2Fwww.foodingfactory.com%2Fgoods%2Fbest.php&amp;state=9cd077e6ebb76da5ccf25819b39b6736"
+                >
+                  <img
+                    src="https://thenaum.cdn-nhncommerce.com/data/skin/front/moment/img/member/icon_naver.png"
+                    alt="naver 아이디 로그인"
+                  />
+                </a>
+
+                <a href="#" class="btn_apple_login js_btn_apple_login">
+                  <img
+                    id="apple_btn"
+                    src="https://thenaum.cdn-nhncommerce.com/data/skin/front/moment/img/member/icon_apple.png"
+                    alt="Apple 아이디 로그인"
+                  />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -303,7 +249,7 @@ input[type="radio"] + label {
   padding: 0.375rem 0 0 0;
 }
 
-.member_login_box button[type="submit"] {
+.member_login_box button.btn_submit {
   height: 3.5rem;
   background-color: #ff7400;
   border-radius: 0.5rem;
@@ -386,78 +332,6 @@ input[type="radio"] + label {
 .easy_login .sns_list img {
   width: 3.25rem;
   height: 3.25rem;
-}
-
-.member_cont .nonmember_order_box {
-  display: flex;
-  flex-direction: column;
-  gap: 1.25rem;
-  margin: 0.75rem 0 0 0;
-}
-
-.member_cont .nonmember_order_box h3 {
-  font-size: 1.125rem;
-  line-height: 1.125rem;
-  font-weight: 400;
-  color: #222222;
-}
-
-.member_cont .nonmember_order_box .order_input_sec {
-  display: flex;
-  flex-direction: column;
-  gap: 1.25rem;
-  overflow: hidden;
-}
-
-.member_cont .nonmember_order_box .order_input_sec > div {
-  float: left;
-}
-
-.member_cont .nonmember_order_box .input_sec {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
-
-.member_cont .nonmember_order_box .input_sec input {
-  margin: 0;
-  border: none;
-  width: 100%;
-  padding: 0 1rem;
-  height: 3.5rem;
-  background-color: #f2f2f2;
-  border-radius: 0.5rem;
-  font-size: 1rem;
-  display: block;
-  color: #979d9d;
-}
-
-.member_cont .nonmember_order_box p {
-  background: none;
-  padding: 0;
-  font-size: 0.875rem;
-  line-height: 170%;
-  color: #595959;
-  text-align: center;
-}
-
-.member_cont .nonmember_order_box p {
-  padding: 0 0 0 1.125rem;
-  background: url(https://thenaum.cdn-nhncommerce.com/data/skin/front/moment/img/member/icon_caution.png)
-    no-repeat 0 0.1875rem;
-  color: #6d6d6d;
-}
-
-.member_cont .nonmember_order_box .order_input_sec button {
-  margin: 0;
-  float: none;
-  width: 100%;
-  height: 3.5rem;
-  border-radius: 0.5rem;
-  border: 0.0625rem solid #ff7400;
-  color: #ff7400;
-  font-size: 1.125rem;
-  font-weight: 500;
 }
 
 .sub_content:after {

161f58a48eba58f676b41d202a87552e9c9be32a
yeon7485 <wsx4379@naver.com>
2024-12-30 17:22:42 +0900

    [Feature] 구현중

---
+++ src/pages/mypage/client/CreateReview.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { useReviewStore } from '../../../stores/useReviewStore';
+import ProductItem from './components/ProductItem.vue';
+
+const reviewStore = useReviewStore();
+
+</script>
+
+<template>
+  <div>
+    <div class="review_container">
+    <h1>리뷰 작성</h1>
+    <div class="line"></div>
+    <div class="review_box">
+      <ProductItem :product="reviewStore.product"  />
+
+
+    <div class="input_box">
+      <p>제목</p>
+      <input type="text">
+    </div>
+    
+    <div  class="input_box">
+      <p>내용</p>
+      <textarea name="review"></textarea>
+    </div>
+
+    <div  class="input_box">
+      <p>사진 등록</p>
+      <input type="file" name="" id="">
+    </div>
+  </div>
+  </div>
+      </div>
+</template>
+
+<style scoped>
+.review_container > h1 {
+  font-size: 1.5rem;
+  margin-bottom: 1.5rem;
+}
+
+.line {
+  width: 100%;
+  height: 2px;
+  background-color: #ccc;
+}
+
+.review_box {
+  width: 100%;
+  padding: 1.5rem 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+.input_box * {
+  width: 100%;
+}
+
+
+</style>
--- src/stores/useReviewStore.js
+++ src/stores/useReviewStore.js
@@ -2,7 +2,16 @@ import axios from "axios";
 import { defineStore } from "pinia";
 
 export const useReviewStore = defineStore("review", {
-  state: () => ({}),
+  state: () => ({
+    store: {},
+    product: {
+      name: "마라떡볶이",
+      price: 14000,
+      orderDate: "24.12.20",
+      image:
+        "https://thenaum.cdn-nhncommerce.com/data/goods/15/01/26/1000000463/1000000463_magnify_07.jpg",
+    },
+  }),
 
   actions: {
     async getStoreReview(storeId = 1) {
